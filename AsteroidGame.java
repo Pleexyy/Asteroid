d3660650225f6fa1b50c015d6282e36a1bf1886b
Thibaud <thibaud.bonneville@gmail.com>
2020-05-22 18:31:23 +0200

    Add files via upload

--- AsteroidGame.java
+++ AsteroidGame.java
@@ -8,19 +8,26 @@ import java.awt.event.KeyListener;
 public class AsteroidGame extends JPanel implements KeyListener {
     private static final long serialVersionUID = 1L;
     private int rotation = 0;
-    // private int[] x = { 30, 10, 50};
-    // private int[] y = { 10, 70, 70};
-    int cntrX = getWidth() / 2;
-    int cntrY = getHeight() / 2;
+    private int x1, x2, x3, y1, y2, y3;
 
-    int x1 = cntrX, x2 = cntrX - 20, x3 = cntrX + 20;
-    int y1 = cntrY - 60, y2 = cntrY + 15, y3 = cntrY + 15;
-
-    public AsteroidGame() {
+    public AsteroidGame(int width, int height) {
         super();
         addKeyListener(this);
         this.setFocusable(true);
         setBackground(Color.BLACK);
+
+        int cntrX = width / 2;
+        int cntrY = height / 2;
+
+        x1 = cntrX;
+        x2 = cntrX - 20; 
+        x3 = cntrX + 20;
+
+        y1 = cntrY - 60; 
+        y2 = cntrY + 15;
+        y3 = cntrY + 15;
+        System.out.println(cntrX);
+        System.out.println(cntrY);
     }
 
     @Override
@@ -53,10 +60,15 @@ public class AsteroidGame extends JPanel implements KeyListener {
             x3 -= 5;
             this.repaint();
         }
-        if (k == KeyEvent.VK_RIGHT) {
+        if (k == KeyEvent.VK_RIGHT && x3 != getWidth()) {
             x1 += 5;
             x2 += 5;
             x3 += 5;
+            this.repaint();
+        } else if (x3 == getWidth()) {
+            x1 = 30;
+            x2 = 10;
+            x3 = 50;
             this.repaint();
         }
     }
@@ -71,6 +83,7 @@ public class AsteroidGame extends JPanel implements KeyListener {
 
     @Override
     public void paintComponent(Graphics g) {
+
         Graphics2D g2d = (Graphics2D) g;
         super.paintComponent(g);
         g2d.setColor(Color.WHITE);
@@ -84,7 +97,7 @@ public class AsteroidGame extends JPanel implements KeyListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         Container contentPane = frame.getContentPane();
-        contentPane.add(new AsteroidGame());
+        contentPane.add(new AsteroidGame(800, 800));
         frame.setVisible(true);
     }
 
