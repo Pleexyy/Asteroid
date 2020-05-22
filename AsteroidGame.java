a40d8cc0381048a5fe2c49292bc4b96b9069c52a
Thibaud <thibaud.bonneville@gmail.com>
2020-05-23 00:00:07 +0200

    Add files via upload

--- AsteroidGame.java
+++ AsteroidGame.java
@@ -8,7 +8,8 @@ import java.awt.event.KeyListener;
 public class AsteroidGame extends JPanel implements KeyListener {
     private static final long serialVersionUID = 1L;
     private int rotation = 0;
-    private int x1, x2, x3, y1, y2, y3;
+    private int x1, x2, x3, y1, y2, y3, locX, locY;
+    private boolean shot = false;
 
     public AsteroidGame(int width, int height) {
         super();
@@ -20,10 +21,10 @@ public class AsteroidGame extends JPanel implements KeyListener {
         int cntrY = height / 2;
 
         x1 = cntrX;
-        x2 = cntrX - 20; 
+        x2 = cntrX - 20;
         x3 = cntrX + 20;
 
-        y1 = cntrY - 60; 
+        y1 = cntrY - 60;
         y2 = cntrY + 15;
         y3 = cntrY + 15;
         System.out.println(cntrX);
@@ -32,7 +33,6 @@ public class AsteroidGame extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("keyPressed");
         int k = e.getKeyCode();
         if (k == KeyEvent.VK_A) {
             this.rotation += -5;
@@ -42,33 +42,9 @@ public class AsteroidGame extends JPanel implements KeyListener {
             this.rotation += 5;
             this.repaint();
         }
-        if (k == KeyEvent.VK_UP) {
-            y1 -= 5;
-            y2 -= 5;
-            y3 -= 5;
-            this.repaint();
-        }
-        if (k == KeyEvent.VK_DOWN) {
-            y1 += 5;
-            y2 += 5;
-            y3 += 5;
-            this.repaint();
-        }
-        if (k == KeyEvent.VK_LEFT) {
-            x1 -= 5;
-            x2 -= 5;
-            x3 -= 5;
-            this.repaint();
-        }
-        if (k == KeyEvent.VK_RIGHT && x3 != getWidth()) {
-            x1 += 5;
-            x2 += 5;
-            x3 += 5;
-            this.repaint();
-        } else if (x3 == getWidth()) {
-            x1 = 30;
-            x2 = 10;
-            x3 = 50;
+        if (k == KeyEvent.VK_SPACE) {
+            System.out.println("Fire ---");
+            shot = true;
             this.repaint();
         }
     }
@@ -81,14 +57,28 @@ public class AsteroidGame extends JPanel implements KeyListener {
     public void keyTyped(KeyEvent e) {
     }
 
-    @Override
     public void paintComponent(Graphics g) {
-
         Graphics2D g2d = (Graphics2D) g;
         super.paintComponent(g);
         g2d.setColor(Color.WHITE);
         g2d.rotate(Math.toRadians(this.rotation), (x1 + x2 + x3) / 3, (y1 + y2 + y3) / 3);
         g2d.drawPolygon(new int[] { x1, x2, x3 }, new int[] { y1, y2, y3 }, 3);
+    }
+
+    public void drawLaser(Graphics g) {
+        super.paintComponents(g);
+        locX = x1;
+        locY = y1 - 10;
+        g.setColor(Color.WHITE);
+        g.drawLine(locX, locY, locX, locY);
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        // tir si la touche est activée
+        if (shot == true) {
+            drawLaser(g);
+        }
     }
 
     public static void main(String[] args) {
@@ -98,6 +88,9 @@ public class AsteroidGame extends JPanel implements KeyListener {
         frame.setResizable(false);
         Container contentPane = frame.getContentPane();
         contentPane.add(new AsteroidGame(800, 800));
+        // permet de centrer la frame au milieu de l'écran
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height / 2 - frame.getSize().height / 2);
         frame.setVisible(true);
     }
 
